340e35d9101a34ad2bd74f6546276b06505ce915
Mark Wecke <wecke@neoskop.de>
2024-10-15 10:56:30 +0200

    feat(cli): switch to esm build

--- packages/cli/test/commands/hello/index.test.ts
+++ packages/cli/test/commands/hello/index.test.ts
@@ -1,12 +1,11 @@
-import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { runCommand } from '@oclif/test';
+import { expect } from 'chai';
 
 describe('hello', () => {
     it('runs hello', async () => {
         const { stdout } = await runCommand('hello friend --from oclif');
 
-        assert(stdout.includes('hello friend from oclif!'));
-        // expect(stdout).to.contain('hello friend from oclif!');
+        expect(stdout).to.include('hello friend from oclif!');
     });
 });
--- packages/cli/test/commands/hello/world.test.ts
+++ packages/cli/test/commands/hello/world.test.ts
@@ -1,11 +1,10 @@
-import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { runCommand } from '@oclif/test';
+import { expect } from 'chai';
 
 describe('hello world', () => {
     it('runs hello world cmd', async () => {
         const { stdout } = await runCommand('hello world');
-        assert(stdout.includes('hello world!'));
-        // expect(stdout).to.contain('hello world!');
+        expect(stdout).to.include('hello world!');
     });
 });
